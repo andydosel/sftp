7dda40ff6c46de36c0c27d77d8b92fc2c6a00b64
Aendi <andydosel@outlook.com>
2018-10-15 20:51:38 +0800

    second version

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import tkinter
+from tkinter import *
+from tkinter import filedialog
 import paramiko
 
-def onhit():
+def checktheshit():
 	try:
 		ip = ipadress.get()
 		usr = usrname.get()
@@ -11,47 +12,55 @@ def onhit():
 		ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
 		ssh_client.connect(ip, 22, usr, psw)
 		std_in, std_out, std_err = ssh_client.exec_command("ls -l")
+		checkbutton.configure(text = 'SUCCESS')
+		checkbutton.configure(state = 'disabled')
 		root.update()
 		root.deiconify()
 		for line in std_out:
 			print(line.strip("\n"))
 			
 	except:
+		checkbutton.configure(text = 'FAILED')
 		root.deiconify()
-		
-
 	
-def on_hit():
+def senttheshit():
 	try:
 		ip = ipadress.get()
 		usr = usrname.get()
 		psw = pasword.get()
+		servicef = servicefile.get()
 		root.withdraw()
 		ssh_client = paramiko.Transport((ip, 22))
 		ssh_client.connect(username = usr, password = psw)
 		sftp = paramiko.SFTPClient.from_transport(ssh_client)
-		sftp.put('X:\main.py', 'main.py')
+		sftp.put(getfilename, servicef)
 		ssh_client.close()
 		print('success')
 		root.deiconify()
 	except:
 		root.deiconify()
 		
+def choosetheshit():
+	global getfilename
+	getfilename = filedialog.askopenfilename()
+	filename.configure(text = getfilename)
 
-root = tkinter.Tk()
-root.geometry('250x100')
-root.title('fuck')
+root = Tk()
+root.geometry('300x150')
+root.title('sftp')
 
-var = tkinter.StringVar()
-
-iplabel = tkinter.Label(root, text = 'ip_address:')
-usrlabel = tkinter.Label(root, text = 'username:')
-pswlabel = tkinter.Label(root, text = 'password:')
-ipadress = tkinter.Entry(root)
-usrname = tkinter.Entry(root)
-pasword = tkinter.Entry(root, show = '*')
-checkbutton = tkinter.Button(root, text = 'CHECK', command = onhit)
-sentbutton = tkinter.Button(root, text = 'SENT', command = on_hit)
+iplabel = Label(root, text = 'ip_address:')
+usrlabel = Label(root, text = 'user_name:')
+pswlabel = Label(root, text = 'password:')
+filename = Label(root, text = 'NULL')
+servicefilelabel = Label(root,text = 'expected filename:')
+ipadress = Entry(root)
+usrname = Entry(root)
+pasword = Entry(root, show = '*')
+servicefile = Entry(root)
+checkbutton = Button(root, text = 'CHECK', command = checktheshit)
+sentbutton = Button(root, text = 'SENT', command = senttheshit)
+choosebutton = Button(root, text = 'CHOOSE', command = choosetheshit)
 
 iplabel.grid(row = 0)
 ipadress.grid(row = 0, column = 1)
@@ -59,6 +68,10 @@ usrlabel.grid(row = 1)
 usrname.grid(row = 1, column = 1)
 pswlabel.grid(row = 2)
 pasword.grid(row = 2, column = 1)
-checkbutton.grid(row = 3)
-sentbutton.grid(row = 3, column = 1)
+choosebutton.grid(row = 3)
+filename.grid(row = 3, column = 1)
+servicefilelabel.grid(row = 4)
+servicefile.grid(row = 4, column = 1)
+checkbutton.grid(row = 5)
+sentbutton.grid(row = 5, column = 1)
 root.mainloop()
